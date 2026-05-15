0c490670feced895ae0e2bbd4269982a56c289e3
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-05-15 10:23:42 +0500

    add pull step in frontend workflow

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div className='font-serif text-center font-bold'>Under Development</div>
+        <div className='font-serif text-center font-bold underline'>Under Development</div>
     )
 }
 
