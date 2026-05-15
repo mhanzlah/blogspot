58257d88eadd6e1213c82241a86a97234048f992
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-05-15 09:13:10 +0500

    test vercel deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>Under Development</div>
-  )
+    return (
+        <div>Under Development (Testing Vercel Deployment)</div>
+    )
 }
 
 export default App
