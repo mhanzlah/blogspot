2d1192a948f9dab2669d04154f052f84d6abebbb
Muhammad Hanzla <sayhi.hanzla@gmail.com>
2026-05-20 12:00:12 +0500

    add API_URL log in AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -6,6 +6,9 @@ export const AuthContext = createContext()
 export const AuthProvider = ({ children }) => {
     const API_URL = import.meta.env.VITE_API_URL
 
+    console.log(API_URL);
+
+
     const [user, setUser] = useState(null)
     const [accessToken, setAccessToken] = useState(null)
     const [loading, setLoading] = useState(true)
